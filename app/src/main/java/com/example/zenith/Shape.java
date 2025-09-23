783666e47e0e4eecf8cd03e11a5aebbfeebafdc0
lkpaquin-coder <lkpaquin@ualberta.ca>
2025-09-23 17:43:53 -0600

    Update Shape.java

--- app/src/main/java/com/example/zenith/Shape.java
+++ app/src/main/java/com/example/zenith/Shape.java
@@ -1,6 +1,7 @@
+package com.example.zenith;
 
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "aquamarine";
+    protected String color = "Pink";
 }
