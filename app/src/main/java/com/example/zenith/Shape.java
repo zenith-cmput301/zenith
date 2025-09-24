e52860b7dca58dbd69c699a43eec41c434e6181d
emegriffioen <emegriffioen@gmail.com>
2025-09-23 18:33:49 -0600

    Cleaned up

--- app/src/main/java/com/example/zenith/Shape.java
+++ app/src/main/java/com/example/zenith/Shape.java
@@ -2,10 +2,5 @@ package com.example.zenith;
 public abstract class Shape {
     protected int x;
     protected int y;
-<<<<<<< HEAD
     protected String color = "teal";
 }
-=======
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
-}
->>>>>>> origin
